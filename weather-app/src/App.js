3e87ecc6f61672b97d29cfc216a1f8834b2c01c5
SXV357 <shrevis2018@gmail.com>
2022-08-14 22:06:43 -0500

    initial commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react'
+import API_KEY from './apikey'
+
+export default function App(){
+
+const API_Info = {
+  key: {API_KEY},
+  link: "http://api.weatherapi.com/v1"
+}
+
+  const [currentWeather, setCurrentWeather] = useState({})
+  const [input, setInput] = useState(" ")
+
+
+  function getData(event){
+    if (event.key === "Enter") {
+    fetch(`${API_Info.link}/current.json?key=${API_Info.key}&q=${input}`)
+      .then(res => res.json())
+      .then(data => {setCurrentWeather(data); setInput(" ")})
+  }
+  }
+
+  function determineDayOrNight(t){
+    let AmOrPm;
+    if (t.slice(11,13)  === "00") {
+      AmOrPm = "PM"
+    }
+    else{
+      if (t.slice(11,13) < "12") {
+        AmOrPm = "AM"
+      }
+      else{
+        AmOrPm = "PM"
+      }
+    }
+    return AmOrPm
+  }
+
+  function representDayMonthYear(t){
+    let day = t.slice(8,10)
+    let month = t.slice(5,7)
+    let year = t.slice(0,4)
+    return `${day}/${month}/${year}`
+  }
+
+
+  return(
+    <main>
+      <div className = "input-container">
+        <input 
+            type = "text" 
+            placeholder = "Search..." 
+            onChange = {(event) => {
+              const {value} = event.target
+              setInput(value)
+            }} 
+            value = {input} 
+            onKeyPress = {getData}/>
+            </div>
+        {currentWeather.current ? <div className = "weather-container">
+          <p>{currentWeather.location.name}, {currentWeather.location.region}, {currentWeather.location.country}</p>
+          <p>{representDayMonthYear(currentWeather.location.localtime)} {currentWeather.location.localtime.slice(11, 16)} {determineDayOrNight(currentWeather.location.localtime)}</p>
+          <p>{currentWeather.current.temp_f}°F({currentWeather.current.temp_c}°C)</p>
+          <p>Feels like: {currentWeather.current.feelslike_f}°F({currentWeather.current.feelslike_c}°C)</p>
+          <p>{currentWeather.current.condition.text}</p>
+          <img src = {currentWeather.current.condition.icon}/>
+        </div>: " "}
+    </main>
+  )
+}
---
+++ weather-app/src/testapi.js
@@ -0,0 +1,3 @@
+const API_Key = 'e50e77522d49497c862234623220108';
+
+export default API_KEY;
